3235be7356b63b30438a8a5b5058d5d46a887583
dyminki <barbara.dyminkiewicz@gmail.com>
2018-07-20 13:05:36 +0200

    change path 2

--- js/cards/katakana.js
+++ js/cards/katakana.js
@@ -1,75 +1,75 @@
 module.exports = [
     {
         id: 0,
-        url: '../images/katakana/a.png',
+        url: './images/katakana/a.png',
     },
     {
         id: 1,
-        url: '../images/katakana/i.png',
+        url: './images/katakana/i.png',
     },
     {
         id: 2,
-        url: '../images/katakana/u.png',
+        url: './images/katakana/u.png',
     },
     {
         id: 3,
-        url: '../images/katakana/e.png',
+        url: './images/katakana/e.png',
     },
     {
         id: 4,
-        url: '../images/katakana/o.png',
+        url: './images/katakana/o.png',
     },
     {
         id: 5,
-        url: '../images/katakana/ka.png',
+        url: './images/katakana/ka.png',
     },
     {
         id: 6,
-        url: '../images/katakana/ki.png',
+        url: './images/katakana/ki.png',
     },
     {
         id: 7,
-        url: '../images/katakana/ku.png',
+        url: './images/katakana/ku.png',
     },
     {
         id: 8,
-        url: '../images/katakana/ke.png',
+        url: './images/katakana/ke.png',
     },
     {
         id: 9,
-        url: '../images/katakana/ko.png',
+        url: './images/katakana/ko.png',
     },
     {
         id: 10,
-        url: '../images/katakana/sa.png',
+        url: './images/katakana/sa.png',
     },
     {
         id: 11,
-        url: '../images/katakana/shi.png',
+        url: './images/katakana/shi.png',
     },
     {
         id: 12,
-        url: '../images/katakana/su.png',
+        url: './images/katakana/su.png',
     },
     {
         id: 13,
-        url: '../images/katakana/se.png',
+        url: './images/katakana/se.png',
     },
     {
         id: 14,
-        url: '../images/katakana/so.png',
+        url: './images/katakana/so.png',
     },
     {
         id: 15,
-        url: '../images/katakana/ta.png',
+        url: './images/katakana/ta.png',
     },
     {
         id: 16,
-        url: '../images/katakana/chi.png',
+        url: './images/katakana/chi.png',
     },
     {
         id: 17,
-        url: '../images/katakana/tsu.png',
+        url: './images/katakana/tsu.png',
     },
     {
         id: 18,
@@ -77,111 +77,111 @@ module.exports = [
     },
     {
         id: 19,
-        url: '../images/katakana/to.png',
+        url: './images/katakana/to.png',
     },
     {
         id: 20,
-        url: '../images/katakana/na.png',
+        url: './images/katakana/na.png',
     },
     {
         id: 21,
-        url: '../images/katakana/ni.png',
+        url: './images/katakana/ni.png',
     },
     {
         id: 22,
-        url: '../images/katakana/nu.png',
+        url: './images/katakana/nu.png',
     },
     {
         id: 23,
-        url: '../images/katakana/ne.png',
+        url: './images/katakana/ne.png',
     },
     {
         id: 24,
-        url: '../images/katakana/no.png',
+        url: './images/katakana/no.png',
     },
     {
         id: 25,
-        url: '../images/katakana/ha.png',
+        url: './images/katakana/ha.png',
     },
     {
         id: 26,
-        url: '../images/katakana/hi.png',
+        url: './images/katakana/hi.png',
     },
     {
         id: 27,
-        url: '../images/katakana/fu.png',
+        url: './images/katakana/fu.png',
     },
     {
         id: 28,
-        url: '../images/katakana/he.png',
+        url: './images/katakana/he.png',
     },
     {
         id: 29,
-        url: '../images/katakana/ho.png',
+        url: './images/katakana/ho.png',
     },
     {
         id: 30,
-        url: '../images/katakana/ma.png',
+        url: './images/katakana/ma.png',
     },
     {
         id: 31,
-        url: '../images/katakana/mi.png',
+        url: './images/katakana/mi.png',
     },
  
     {
         id: 32,
-        url: '../images/katakana/mu.png',
+        url: './images/katakana/mu.png',
     },
     {
         id: 33,
-        url: '../images/katakana/me.png',
+        url: './images/katakana/me.png',
     },
     {
         id: 34,
-        url: '../images/katakana/mo.png',
+        url: './images/katakana/mo.png',
     },
     {
         id: 35,
-        url: '../images/katakana/ra.png',
+        url: './images/katakana/ra.png',
     },
     {
         id: 36,
-        url: '../images/katakana/ri.png',
+        url: './images/katakana/ri.png',
     },
     {
         id: 37,
-        url: '../images/katakana/ru.png',
+        url: './images/katakana/ru.png',
     },
     {
         id: 38,
-        url: '../images/katakana/re.png',
+        url: './images/katakana/re.png',
     },
     {
         id: 39,
-        url: '../images/katakana/ro.png',
+        url: './images/katakana/ro.png',
     },
     {
         id: 40,
-        url: '../images/katakana/ya.png',
+        url: './images/katakana/ya.png',
     },
     {
         id: 41,
-        url: '../images/katakana/yo.png',
+        url: './images/katakana/yo.png',
     },
     {
         id: 42,
-        url: '../images/katakana/yu.png',
+        url: './images/katakana/yu.png',
     },
     {
         id: 43,
-        url: '../images/katakana/wa.png',
+        url: './images/katakana/wa.png',
     },
     {
         id: 44,
-        url: '../images/katakana/wo.png',
+        url: './images/katakana/wo.png',
     },
     {
         id: 45,
-        url: '../images/katakana/n.png',
+        url: './images/katakana/n.png',
     },
 ]
